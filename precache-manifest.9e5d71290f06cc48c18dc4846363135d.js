13a86863fce4379af274a134a4946323b8eacfad
punkbear <punkbear@126.com>
2020-05-14 15:09:41 +0800

    Updates

---
+++ precache-manifest.9e5d71290f06cc48c18dc4846363135d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8091738401424b639152a83f997d6542",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "29d77453cff6af2974ef",
+    "url": "/react-tutorial/static/css/main.53574754.chunk.css"
+  },
+  {
+    "revision": "7dd248e09ede03dbc280",
+    "url": "/react-tutorial/static/js/2.480cb2de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.480cb2de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d77453cff6af2974ef",
+    "url": "/react-tutorial/static/js/main.f667e86b.chunk.js"
+  },
+  {
+    "revision": "8cacb06cb51a1d6598ce",
+    "url": "/react-tutorial/static/js/runtime-main.5641d20e.js"
+  }
+]);
